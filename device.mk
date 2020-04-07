3c2a18421f8c88b35cbd2124b1b8918f077e2aa0
MArshadR <mohamedarshadr@gmail.com>
2020-04-07 10:27:37 +0000

    Onclite: used GCam as Snap is derped in source

--- device.mk
+++ device.mk
@@ -122,7 +122,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libgui_vendor \
-    Snap
+    GCam
 
 # Configstore
 PRODUCT_PACKAGES += \
